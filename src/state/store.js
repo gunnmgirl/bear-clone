c13900d46db0bcb531cfd01395b5a199dc5af09b
gunnmgirl <48892214+gunnmgirl@users.noreply.github.com>
2020-05-04 00:06:12 +0200

    Persists the state of the application in the localStorage

--- src/state/store.js
+++ src/state/store.js
@@ -1,7 +1,13 @@
 import { createStore } from "redux";
 
 import rootReducer from "../state/rootReducer";
+import { loadState, saveState } from "./localStorage";
 
-const store = createStore(rootReducer);
+const persistedState = loadState();
+const store = createStore(rootReducer, persistedState);
+
+store.subscribe(() => {
+  saveState({ notes: store.getState().notes });
+});
 
 export default store;
